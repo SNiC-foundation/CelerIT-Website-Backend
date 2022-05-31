636cd624fbcdbe87b824d771f7b69d2ba410fa57
Wutru <wvdh99@gmail.com>
2022-05-31 10:57:50 +0200

    Removed console print

--- src/database/dataSource.ts
+++ src/database/dataSource.ts
@@ -4,7 +4,6 @@ import path from 'path';
 let AppDataSource: DataSource;
 
 export async function initializeDataSource(): Promise<DataSource> {
-  console.error(process.env);
   let options: DataSourceOptions;
 
   switch (process.env.TYPEORM_CONNECTION) {
