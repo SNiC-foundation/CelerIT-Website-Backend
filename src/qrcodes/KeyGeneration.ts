b59350de5fd8a26072e7747a0b208063eaab33a2
Wutru <wvdh99@gmail.com>
2022-07-23 15:45:11 +0200

    Testing encryption

--- src/qrcodes/KeyGeneration.ts
+++ src/qrcodes/KeyGeneration.ts
@@ -1,14 +1,14 @@
 import * as forge from 'node-forge';
 import * as fs from 'fs';
 
-const keypair = forge.pki.rsa.generateKeyPair({ bits: 512 });
+const keypair = forge.pki.rsa.generateKeyPair({ bits: 128 });
 
 const toJSONFile = JSON.stringify({
   publicKeyPem: forge.pki.publicKeyToPem(keypair.publicKey),
   privateKeyPem: forge.pki.privateKeyToPem(keypair.privateKey),
 });
 
-fs.writeFile('../../../src/qrcodes/keys.txt', toJSONFile, (err) => {
+fs.writeFile('../../../src/qrcodes/keys.json', toJSONFile, (err) => {
   if (err) {
     console.log(err);
   }
--- src/qrcodes/OfflineQRGeneration.ts
+++ src/qrcodes/OfflineQRGeneration.ts
@@ -1,6 +1,16 @@
 import QRCode from 'qrcode';
+import * as forge from 'node-forge';
+import keys from './keys.json';
 
-const encryptedId = 'faoufhuhuhjrhh';
+// sign data with a private key and output DigestInfo DER-encoded bytes
+// (defaults to RSASSA PKCS#1 v1.5)
+const md = forge.md.sha1.create();
+
+// const publicKey = forge.pki.publicKeyFromPem(keys.publicKeyPem);
+const privateKey = forge.pki.privateKeyFromPem(keys.privateKeyPem);
+
+md.update('sign this', 'utf8');
+const encryptedId = privateKey.sign(md);
 
 QRCode.toFile('../../../src/qrcodes/generated/foo.png', encryptedId, {
   color: {
