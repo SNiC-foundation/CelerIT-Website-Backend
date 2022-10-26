ecc44f6d7310706c876e409b28fe4cc0ed47a5e5
Roy Kakkenberg <me@roykakkenberg.nl>
2022-10-26 14:54:52 +0200

    Fix RBAC not actually blocking any endpoints

--- src/authentication/Authentication.ts
+++ src/authentication/Authentication.ts
@@ -1,5 +1,7 @@
 import express from 'express';
 import { ApiError, HTTPStatus } from '../helpers/error';
+import { getDataSource } from '../database/dataSource';
+import User from '../entities/User';
 
 /**
  * Express Authentication function that checks if user can access resource.
@@ -20,18 +22,18 @@ export async function expressAuthentication(
         throw new ApiError(HTTPStatus.Unauthorized, 'You are not logged in.');
       }
 
-      // // If any roles are defined, check them
-      // if (scopes !== undefined && scopes.length > 0) {
-      //   const user = (await AppDataSource.getRepository(User)
-      //     .findOne({ where: { id: (request.user as User).id }, relations: ['roles'] }))!;
-      //
-      //   // Compute if there is an intersection
-      //   const intersect = user.roles.some((r) => scopes.find((s) => s === r.name) !== undefined);
-      //   // If the intersection of the roles is non-empty, the user is authorized
-      //   if (!intersect) {
-      //     throw new ApiError(HTTPStatus.Unauthorized, 'You don\'t have permission to do this.');
-      //   }
-      // }
+      // If any roles are defined, check them
+      if (scopes !== undefined && scopes.length > 0) {
+        const user = (await getDataSource().getRepository(User)
+          .findOne({ where: { id: (request.user as User).id }, relations: ['roles'] }))!;
+
+        // Compute if there is an intersection
+        const intersect = user.roles.some((r) => scopes.find((s) => s === r.name) !== undefined);
+        // If the intersection of the roles is non-empty, the user is authorized
+        if (!intersect) {
+          throw new ApiError(HTTPStatus.Unauthorized, 'You don\'t have permission to do this.');
+        }
+      }
       return request.user;
     }
     default: throw new Error('Unknown security scheme');
